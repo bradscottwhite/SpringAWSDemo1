651e94d5c82d0dbaf935ff92ad61cf400573d3eb
Brad White <bradscottwhite@gmail.com>
2022-10-31 23:02:34 -0400

    added cors

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -7,27 +7,29 @@ import com.example.repository.EmployeeRepository;
 import com.example.entity.Employee;
 
 @RestController
+@RequestMapping("employee")
+@CrossOrigin
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
-	@PostMapping("/employee")
+	@PostMapping("/")
 	public Employee saveEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
 
-	@GetMapping("/employee/{id}")
+	@GetMapping("/{id}")
 	public Employee getEmployee(@PathVariable("id") String employeeId) {
 		return employeeRepository.getEmployeeById(employeeId);
 	}
 
-	@DeleteMapping("/employee/{id}")
+	@DeleteMapping("/{id}")
 	public String deleteEmployee(@PathVariable("id") String employeeId) {
 		return employeeRepository.delete(employeeId);
 	}
 
-	@PutMapping("/employee/{id}")
+	@PutMapping("/{id}")
 	public String updateEmployee(@PathVariable("id") String employeeId, @RequestBody Employee employee) {
 		return employeeRepository.update(employeeId, employee);
 	}
